1ec394e1c514d1bfc5160475f2f686ef6b0cf438
JomiXedYu <JomiXedYu@outlook.com>
2021-06-21 21:58:00 +0800

    Update EngineManager.h

--- include/SapphireEngine/EngineManager.h
+++ include/SapphireEngine/EngineManager.h
@@ -2,11 +2,8 @@
 #define SAPPHIREENGINE_ENGINEMANAGER_H
 
 #include <SapphireEngine/Private/BaseInterface.h>
-
 #include <SapphireEngine/Screen.h>
-
 #include <SapphireEngine/InputDevice/Input.h>
-
 #include <SapphireEngine/UnitType/Vector2.h>
 #include <SapphireEngine/Application.h>
 #include <SapphireEngine/InputDevice/KeyAxisDevice.h>
@@ -14,15 +11,13 @@
 
 namespace SapphireEngine
 {
-    using namespace Private;
-    using namespace InputDevice;
-
     namespace EngineManager
     {
         static void Init()
         {
             Application::Initialize(Vector2(800.0f, 800.0f));
 
+            using namespace SapphireEngine::InputDevice;
             Input::AddDeivce(new KeyAxisDevice("horizontal", 0.08f, KeyCode::A, KeyCode::D));
             Input::AddDeivce(new KeyAxisDevice("vertical", 0.08f, KeyCode::W, KeyCode::S));
             Input::AddDeivce(new MouseAxisDevice("mouseX", 0.0f, MouseAxisCode::MouseX));
@@ -31,7 +26,7 @@ namespace SapphireEngine
         static void Term()
         {
             Application::Terminate();
-            Input::Terminate();
+            InputDevice::Input::Terminate();
         }
     };
 }

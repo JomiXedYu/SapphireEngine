9554d6ec7b199aee7677cbd93155d901c23088e5
JomiXedYu <JomiXedYu@outlook.com>
2021-06-21 18:46:56 +0800

    Logger

--- include/SapphireEngine/Logger.h
+++ include/SapphireEngine/Logger.h
@@ -2,6 +2,8 @@
 #define SAPPHIREENGINE_LOGGER_H
 
 #include <CoreLib/String.h>
+#include <iostream>
+
 namespace SapphireEngine
 {
     using namespace JxCoreLib;
@@ -9,10 +11,13 @@ namespace SapphireEngine
     class Logger
     {
     public:
-        static void Info(const string& log);
-        static void Warning(const string& log);
-        static void Error(const string& log);
+        static void PrintInfo(const string& log);
+        static void PrintWarning(const string& log);
+        static void PrintError(const string& log);
 
+        static std::ostream& Info();
+        static std::ostream& Warning();
+        static std::ostream& Error();
     };
 
 }
--- src/SapphireEngine/Logger.cpp
+++ src/SapphireEngine/Logger.cpp
@@ -4,16 +4,32 @@
 namespace SapphireEngine
 {
     using namespace std;
-    void Logger::Info(const string& log)
+    void Logger::PrintInfo(const string& log)
     {
         cout << "[INFO]" << log << endl;
     }
-    void Logger::Warning(const string& log)
+    void Logger::PrintWarning(const string& log)
     {
         cout << "[WARNING]" << log << endl;
     }
-    void Logger::Error(const string& log)
+    void Logger::PrintError(const string& log)
     {
         cout << "[ERROR]" << log << endl;
+    }
+
+    ostream& Logger::Info()
+    {
+        cout << endl << "[INFO]";
+        return cout;
+    }
+    ostream& Logger::Warning()
+    {
+        cout << endl << "[WARNING]";
+        return cout;
+    }
+    ostream& Logger::Error()
+    {
+        cout << endl << "[ERROR]";
+        return cout;
     }
 }

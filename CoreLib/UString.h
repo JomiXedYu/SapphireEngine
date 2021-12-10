c179b2c114fdb541f832f6accd70e8382c76cfe6
JomiXedYu <JomiXedYu@outlook.com>
2021-12-10 13:06:30 +0800

    CORELIB

--- CoreLib/UString.h
+++ CoreLib/UString.h
@@ -18,9 +18,11 @@
 
 namespace JxCoreLib
 {
-    using string = std::string;
 
-    struct Char final
+    using string = std::string;
+    using string_view = std::string_view;
+
+    struct u8char final
     {
         char value[8]{ 0 };
         inline static int U8Length(const char* c)
@@ -51,16 +53,16 @@ namespace JxCoreLib
 
             return true;
         }
-        Char() {}
-        Char(const char* c)
+        u8char() {}
+        u8char(const char* c)
         {
             Charcpy(this->value, c);
         }
-        bool operator==(const Char& r)
+        bool operator==(const u8char& r)
         {
             return Charcmp(this->value, r.value);
         }
-        bool operator!=(const Char& r)
+        bool operator!=(const u8char& r)
         {
             return !Charcmp(this->value, r.value);
         }
@@ -71,19 +73,19 @@ namespace JxCoreLib
 
     };
 
-    bool operator==(const Char& left, const string& right);
-    bool operator==(const string& left, const Char& right);
-    bool operator!=(const Char& left, const string& right);
-    bool operator!=(const string& left, const Char& right);
-    string operator+(const Char& left, const string& right);
-    string operator+(const string& left, const Char& right);
+    bool operator==(const u8char& left, const string& right);
+    bool operator==(const string& left, const u8char& right);
+    bool operator!=(const u8char& left, const string& right);
+    bool operator!=(const string& left, const u8char& right);
+    string operator+(const u8char& left, const string& right);
+    string operator+(const string& left, const u8char& right);
 
     struct StringIndexMapping final
     {
     private:
         size_t block_size_;
     public:
-        StringIndexMapping(const string& str, size_t block_size) noexcept(false);
+        StringIndexMapping(string_view str, size_t block_size) noexcept(false);
     public:
         std::vector<int> mapping;
         size_t get_block_size() const;
@@ -93,79 +95,47 @@ namespace JxCoreLib
         size_t GetBlockPos(const size_t& pos) const noexcept(false);
     };
 
-    class StringUtil final
+    namespace StringUtil
     {
-    private:
-        StringUtil() = delete;
-    public:
-        static bool IsLittleEndian() noexcept;
-        static size_t U8Length(const string& str, const size_t& pos) noexcept(false);
-    private:
-        static string _Replace(
-            const char* src, size_t src_len,
-            const char* oldstr, size_t oldstr_len,
-            const char* newstr, size_t newstr_len);
-    public:
-        template<typename TStr, typename Toldstr, typename Tnewstr>
-        static auto Replace(const TStr& src, const Toldstr& oldstr, const Tnewstr& newstr) {
-            return _Replace(
-                ToCharPointer(src), Size(src),
-                ToCharPointer(oldstr), Size(oldstr),
-                ToCharPointer(newstr), Size(newstr));
-        }
-        static Char PosAt(const string& src, const size_t& bytepos);
-        static Char CharAt(const string& src, const size_t& charpos);
-        static Char CharAt(const string& src, const size_t& charpos, const StringIndexMapping& mapping);
-        static size_t Length(const string& src);
-        static size_t Length(const string& src, const StringIndexMapping& mapping);
-        static std::vector<uint8_t> GetBytes(const string& str);
+        bool IsLittleEndian() noexcept;
+        size_t U8Length(string_view str, size_t pos) noexcept(false);
+        string Replace(string_view src, string_view oldstr, string_view newstr);
+        u8char PosAt(const string_view& src, const size_t& bytepos);
+        u8char CharAt(const string_view& src, const size_t& charpos);
+        u8char CharAt(const string_view& src, const size_t& charpos, const StringIndexMapping& mapping);
+        size_t Length(const string_view& src);
+        size_t Length(const string_view& src, const StringIndexMapping& mapping);
+        std::vector<uint8_t> GetBytes(const string_view& str);
+        std::u16string Utf8ToUtf16(const string& str);
+        string Utf16ToUtf8(const std::u16string& str);
+        string StringCast(const std::u8string& str);
 
-        static std::u16string Utf8ToUtf16(const string& str);
-        static string Utf16ToUtf8(const std::u16string& str);
+        inline size_t Size(const char* str) { return ::strlen(str); }
+        inline size_t Size(const string& str) { return str.length(); }
+        inline size_t Size(string_view str) { return str.length(); }
 
-        static string StringCast(const std::u8string& str);
+        template<typename... TArgs>
+        constexpr inline size_t Sum(TArgs... args) { return (arg + ... + 0); }
 
-        static const char* ToCharPointer(const char* c) {
-            return c;
-        }
-        static const char* ToCharPointer(const string& str) {
-            return str.c_str();
-        }
-        static size_t Size(const string& str) {
-            return str.size();
-        }
-        static size_t Size(const char* str) {
-            return ::strlen(str);
-        }
-    private:
-        static auto _Sum() { return 0; }
-        template<typename T, typename... TArgs>
-        static auto _Sum(T t, TArgs... args) { return t + _Sum(args...); }
+        inline void AppendStr(string& str, const char* nstr) { str.append(nstr); }
+        inline void AppendStr(string& str, const string& nstr) { str.append(nstr); }
+        inline void AppendStr(string& str, string_view nstr) { str.append(nstr); }
 
-        static void _AppendStr(string& str, const char* nstr)
-        {
-            str.append(nstr);
-        }
-        static void _AppendStr(string& str, const string nstr)
-        {
-            str.append(nstr.c_str());
-        }
-
-        static void _Append(string& str) {}
+        inline void Append(string& str) {}
 
         template<typename T, typename... TArgs>
-        static void _Append(string& str, const T& nstr, TArgs&&... args)
+        inline void Append(string& str, const T& nstr, TArgs&&... args)
         {
-            _AppendStr(str, nstr);
-            _Append(str, args...);
+            AppendStr(str, nstr);
+            Append(str, args...);
         }
-    public:
+
         template<typename... T>
-        static string Concat(const T&... args) {
-            size_t size = _Sum(Size(args)...);
+        string Concat(const T&... args) {
+            size_t size = Sum(Size(args)...);
             string str;
             str.reserve(size + 1);
-            _Append(str, args...);
+            Append(str, args...);
             return str;
         }
     };
